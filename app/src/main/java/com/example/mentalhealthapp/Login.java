322918e783cf66b2af11ac343a8dac1fa72d401e
Nugraha <satyaega10@gmail.com>
2020-06-19 21:34:27 +0700

    Added Project

--- app/src/main/java/com/example/mentalhealthapp/Login.java
+++ app/src/main/java/com/example/mentalhealthapp/Login.java
@@ -62,6 +62,7 @@ public class Login extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(Login.this, "Login success", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
                         } else{
                             Toast.makeText(Login.this, "Error!" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                         }
